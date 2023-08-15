558dade40c62b2d687a67989e1e74f07b0db3bf9
mnoferestibrocku <131692985+mnoferestibrocku@users.noreply.github.com>
2023-08-15 09:11:37 -0400

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -1,13 +1,13 @@
-% After installing "Linux Ubuntu 22.04.2 LTS", containing default applications and devoid of any third-party software installations, you need to follow the following steps to install the necessary tools:
+# After installing "Linux Ubuntu 22.04.2 LTS", containing default applications and devoid of any third-party software installations, you need to follow the following steps to install the necessary tools:
 
-%% Step 1 : INSTALL LTTNG
-%% The docs found at https://lttng.org/docs/v2.12/ a good job of explaining how to install LTTng. However we add the commands here:
-%% Add the LTTng Stable 2.12 PPA repository and update the list of packages:
+## Step 1 : INSTALL LTTNG
+## The docs found at https://lttng.org/docs/v2.12/ a good job of explaining how to install LTTng. However we add the commands here:
+## Add the LTTng Stable 2.12 PPA repository and update the list of packages:
 
 apt-add-repository ppa:lttng/stable-2.12
 apt-get update
 
-%% Install the main LTTng 2.12 packages:
+## Install the main LTTng 2.12 packages:
 
 apt-get install lttng-tools
 apt-get install lttng-modules-dkms
@@ -15,4 +15,4 @@ apt-get install liblttng-ust-dev
 apt-get install liblttng-ust-agent-java
 
 
-%% Step 1 : INSTALL JAVA 
+## Step 1 : INSTALL JAVA 
